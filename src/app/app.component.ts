d371ab301df64a38fc5124d0e7cf50af623764cf
Arif <bdboy126@gmail.com>
2022-10-30 16:53:29 +0600

    Project Commit.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,3 +8,31 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'my-bookmarks';
 }
+
+let bookmarks = [
+  {title: 'Google', url:'https://www.google.com'},
+  {title: 'ittefaq', url:'https://www.ittefaq.com.bd/'},
+  {title: 'Prothom-Alo', url:'https://www.prothomalo.com/'}
+];
+
+@Component({
+  selector:'bookmarks',
+  template: `
+  <table>
+      <thead>
+          <th></th>
+          <th></th>
+      </thead>
+      <tbody>
+          <tr *ngFor="let hero of heroes">
+              <td>{{hero.title}}</td>
+              <td>{{hero.url}}</td>
+          </tr>
+      </tbody>
+  </table>
+`
+})
+
+export class AppComponent2 {
+  title = 'bookmarks';
+}
---
+++ src/app/data.ts
@@ -0,0 +1,5 @@
+const bookmarks = [
+    {title: 'Google', url:'www.google.com'},
+    {title: 'ittefaq', url:'https://www.ittefaq.com.bd/'},
+    {title: 'Prothom-Alo', url:'https://www.prothomalo.com/'}
+];
